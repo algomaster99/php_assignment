4490c8306ce4070f8f4aa64cf592f3d3e3b11fe4
Aman Sharma <mannu.poski10@gmail.com>
2018-03-15 18:56:07 +0530

    index.php and signup page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HomePage</title>
+</head>
+<body>
+	<button onclick="location.href = 'login.php'">Login</button>
+	<button onclick="location.href = 'signup.php'">SignUp</button>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SignUp</title>
+</head>
+<body>
+	<form method="POST">
+		Full Name: <input type="text" name="name"><span id="name">fedf</span><br /><br />
+		E-mail ID: <input type="text" name="email"><span id="email"></span><br /><br />
+		Password: <input type="text" name="pass"><span id="pass"></span><br /><br />
+		Confirm Password: <input type="text" name="cnfpass"><span id="cnfpass"></span><br /><br />
+		DOB: <input type="date" name="dob">
+		Gender: <select>
+			<option disabled selected>&lt;Select&gt;</option>
+			<option value="M">Male</option>
+			<option value="F">Female</option>
+			<option value="O">Other</option>
+		</select>
+	</form>
+</body>
+</html>
